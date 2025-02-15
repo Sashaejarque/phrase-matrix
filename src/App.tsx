0ccab0a6461e71a7a9bf3a3bf38e223f7bad936d
Sasha Ejarque <sashaejarque@MacBook-Air-de-Sasha.local>
2025-02-15 17:04:16 -0300

    feat: added husky for run lint and format before a commit and added icon and title for app

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import { PhrasesProvider } from './features/phrase/context/PhrasesContext';
 import PrhaseIndex from './features/phrase/PhraseIndex';
 import '../src/utils/styles.global.css';
+import { useEffect } from 'react';
 
 function App() {
+  useEffect(() => {
+    document.title = 'Phrase App';
+  }, []);
+
   return (
     <PhrasesProvider>
       <PrhaseIndex />
